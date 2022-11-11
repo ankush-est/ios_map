d778a533f8faacc0e717c23b670a83638f00e4b4
Ankush Lokhande <ankush.lokhande@encoresky.com>
2022-11-11 17:14:56 +0530

    Add marker

--- IosMapApp/IosMapApp/ViewController.swift
+++ IosMapApp/IosMapApp/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         self.view = mapView
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
 
